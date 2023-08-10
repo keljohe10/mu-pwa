717815afdc9a228cb0d0f2bab6e7ed19096799d4
kelvin hernandez cabrera <kelhzkb@hotmail.com>
2023-08-10 18:03:40 +0200

    React query

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,26 +1,44 @@
-'use client'
-import React, { useState, useEffect } from "react";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
-const Dashboard = () => {
-  const [data, setData] = useState(null);
+const fetchPost = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  if (!response.ok) {
+    throw new Error("An error occurred");
+  }
+  return response.json();
+};
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  }, []);
+const DashboardContent = () => {
+  const { data, isLoading, error } = useQuery("post", fetchPost, {
+    retry: 1, // Número de reintentos si la consulta falla
+    staleTime: 1000 * 60 * 60 * 24, // Tiempo en milisegundos que los datos se considerarán frescos
+    cacheTime: 1000 * 60 * 60 * 24 * 30, // Tiempo en milisegundos que los datos se mantendrán en caché
+  });
+
+  if (isLoading) return <p>Loading!...</p>;
+  if (error) return <p>An error occurred: {error.message}</p>;
 
   return (
     <div>
-      {data ? (
-        <div>
-          <h1>{data.title}</h1>
-          <p>{data.body}</p>
-        </div>
-      ) : (
-        <p>Loading!...</p>
-      )}
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
     </div>
+  );
+};
+
+const Dashboard = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false, // Evita la actualización cuando la ventana esté enfocada
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <DashboardContent />
+    </QueryClientProvider>
   );
 };
 
--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 const withPWA = require("next-pwa");
-const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-    runtimeCaching,
-  },
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
 });
