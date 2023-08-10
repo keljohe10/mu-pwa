cd82214d62ca45a077f2401157311deabdc1181e
kelvin hernandez cabrera <kelhzkb@hotmail.com>
2023-08-10 18:25:38 +0200

    testv1

--- next.config.js
+++ next.config.js
@@ -3,5 +3,18 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   dest: "public",
   register: true,
-  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/jsonplaceholder\.typicode\.com/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'api-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        },
+        networkTimeoutSeconds: 10,
+      },
+    },
+  ],
 });
